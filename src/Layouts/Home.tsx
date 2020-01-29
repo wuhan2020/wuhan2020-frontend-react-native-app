88b0595e55ccbe7f1b09c24228b53b41320e961a
Fei Liu <liu.feiwood@gmail.com>
2020-01-29 10:02:30 +0100

    chart poc

--- src/Layouts/Home.tsx
+++ src/Layouts/Home.tsx
@@ -1,28 +1,68 @@
-import * as React from 'react';
-import StatusBarSafeLayout from './StatusBarSafeLayout';
+import React, { Component } from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Echarts from 'react-native-echarts-map';
+import { Button } from 'react-native-elements';
 
-import { useNavigation } from 'react-navigation-hooks';
+const { height } = Dimensions.get('window');
 
-import { getToken } from '../utils';
-import { Text, Button } from 'react-native';
+class Map extends Component {
+  render() {
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{b}',
+      },
+      visualMap: {
+        min: 800,
+        max: 50000,
+        text: ['高', '低'],
+        realtime: false,
+        calculable: true,
+        inRange: {
+          color: ['lightskyblue', 'yellow', 'orangered'],
+        },
+      },
+      series: [
+        {
+          name: '中国',
+          type: 'map',
+          map: 'china',
+          selectedMode: 'single', //multiple多选
+          itemStyle: {
+            normal: {
+              label: {
+                show: false,
+                textStyle: {
+                  color: '#231816',
+                },
+              },
+              areaStyle: { color: '#B1D0EC' },
+              color: '#B1D0EC',
+              borderColor: '#bbb', //区块的边框颜色
+            },
+            emphasis: {
+              //鼠标hover样式
+              label: {
+                show: true,
+                textStyle: {
+                  color: '#fa4f04',
+                },
+              },
+            },
+          },
+          data: [{ name: '四川', selected: true }],
+        },
+      ],
+    };
+    return (
+      <View style={{ backgroundColor: 'white' }}>
+        <Echarts option={option} height={height / 3} />
+        <Button title="map works" loading />
+      </View>
+    );
+  }
+}
 
-const secret = '2vekyeam6jsulh46ih63uxn5vsnpqokk';
+Map.navigationOptions = { title: 'Wuhan 2020 ❤️' };
 
-const HomeScreen = () => {
-  const navigation = useNavigation();
-  return (
-    <StatusBarSafeLayout>
-      <Text>{getToken(secret)}</Text>
-      <Button
-        title="To Settings"
-        onPress={() => {
-          navigation.navigate('Setting');
-        }}
-      />
-    </StatusBarSafeLayout>
-  );
-};
-
-HomeScreen.navigationOptions = { title: 'Welcome' };
-
-export default HomeScreen;
+export default Map;
